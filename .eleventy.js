33b0e417ab586cd9fb84d98bc8a74f7fe1ba09f6
Josh Cunningham <josh@joshcanhelp.com>
2020-02-02 06:05:29 -0800

    Add all layouts

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
-  eleventyConfig.addLayoutAlias('home', 'layouts/home.njk');
+  eleventyConfig.addLayoutAlias('gallery', 'layouts/post.njk');
+  eleventyConfig.addLayoutAlias('link', 'layouts/home.njk');
+  eleventyConfig.addLayoutAlias('page', 'layouts/home.njk');
+  eleventyConfig.addLayoutAlias('post', 'layouts/home.njk');
+  eleventyConfig.addLayoutAlias('quote', 'layouts/home.njk');
+  eleventyConfig.addLayoutAlias('status', 'layouts/home.njk');
 
   return {
     dir: {
