6c4a0b13597f319bc2392a396b7188bee11ad088
Josh Cunningham <josh@joshcanhelp.com>
2020-02-02 17:37:40 -0800

    Layout and config file updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
 
+  const postLayouts = [ 'post', 'link', 'quote', 'gallery', 'status' ];
+
   eleventyConfig.addLayoutAlias('gallery', 'layouts/gallery.njk');
   eleventyConfig.addLayoutAlias('link', 'layouts/link.njk');
   eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
@@ -8,6 +10,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias('status', 'layouts/status.njk');
 
   eleventyConfig.setUseGitIgnore(false);
+
+  eleventyConfig.addCollection('postsCollection', function(collection) {
+    const tmpCollection = collection.getAllSorted();
+    return tmpCollection.reverse().filter(function(tpl) {
+      if ( tpl.data.permalink && postLayouts.includes(tpl.data.layout) ) return tpl;
+    });
+  });
 
   return {
     dir: {
