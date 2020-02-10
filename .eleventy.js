78b236be5fa78150b7792533a2a952c8006885c1
Josh Cunningham <josh@joshcanhelp.com>
2020-02-09 21:15:55 -0800

    Layout and config file updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,31 @@
+const moment = require('moment');
+moment.locale('en');
+
 module.exports = function(eleventyConfig) {
 
-  const postLayouts = [ 'post', 'link', 'quote', 'gallery', 'status' ];
+  eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
+
+  const postLayouts = [ 'post', 'link', 'quote', 'gallery' ];
 
   eleventyConfig.addLayoutAlias('gallery', 'layouts/gallery.njk');
   eleventyConfig.addLayoutAlias('link', 'layouts/link.njk');
-  eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
   eleventyConfig.addLayoutAlias('quote', 'layouts/quote.njk');
   eleventyConfig.addLayoutAlias('status', 'layouts/status.njk');
 
   eleventyConfig.setUseGitIgnore(false);
 
+  eleventyConfig.setTemplateFormats([ 'md', 'css' ]);
+
   eleventyConfig.addCollection('postsCollection', function(collection) {
     const tmpCollection = collection.getAllSorted();
     return tmpCollection.reverse().filter(function(tpl) {
       if ( tpl.data.permalink && postLayouts.includes(tpl.data.layout) ) return tpl;
     });
+  });
+
+  eleventyConfig.addFilter('dateformat', function(dateIn) {
+      return moment(dateIn).format('MMM DD, YYYY [at] h:mm a');
   });
 
   return {
