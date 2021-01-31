14847983d08d0c5f1ff5e8cae74c0fa3af3d0270
Josh Cunningham <josh@joshcanhelp.com>
2021-01-31 10:27:48 -0800

    Prettier eleventy file

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,28 @@
-const moment = require('moment');
+const moment = require("moment");
 const htmlmin = require("html-minifier");
 const slugify = require("slugify");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-moment.locale('en');
+moment.locale("en");
 
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
-
   /*
    * Configuration
    */
 
-  eleventyConfig.addPassthroughCopy({ 'images': '_images' });
-  eleventyConfig.addPassthroughCopy({ 'css': '_css' });
-  eleventyConfig.addPassthroughCopy({ 'html': '_html' });
+  eleventyConfig.addPassthroughCopy({ images: "_images" });
+  eleventyConfig.addPassthroughCopy({ css: "_css" });
+  eleventyConfig.addPassthroughCopy({ html: "_html" });
 
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
 
-  eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+  eleventyConfig.addLayoutAlias("page", "layouts/page.njk");
+  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
   eleventyConfig.addLayoutAlias("idea", "layouts/idea.njk");
-  eleventyConfig.addLayoutAlias('anna', 'layouts/anna.njk');
+  eleventyConfig.addLayoutAlias("anna", "layouts/anna.njk");
 
   eleventyConfig.setUseGitIgnore(false);
 
@@ -31,15 +30,15 @@ module.exports = function (eleventyConfig) {
    * Transforms
    */
 
-  eleventyConfig.addTransform("htmlMinifier", function(content, outputPath) {
-    if ( !outputPath.endsWith(".html" ) ) {
+  eleventyConfig.addTransform("htmlMinifier", function (content, outputPath) {
+    if (!outputPath.endsWith(".html")) {
       return content;
     }
 
     return htmlmin.minify(content, {
       useShortDoctype: true,
       removeComments: true,
-      collapseWhitespace: true
+      collapseWhitespace: true,
     });
   });
 
@@ -47,7 +46,7 @@ module.exports = function (eleventyConfig) {
    * Collections
    */
 
-  eleventyConfig.addCollection('rssCollection', function (collection) {
+  eleventyConfig.addCollection("rssCollection", function (collection) {
     const tmpCollection = collection.getAllSorted();
     return tmpCollection.reverse().filter(function (tpl) {
       if (isPublishedPost(tpl.data) && !tpl.data.link_to) {
@@ -56,7 +55,7 @@ module.exports = function (eleventyConfig) {
     });
   });
 
-  eleventyConfig.addCollection('postsCollection', function (collection) {
+  eleventyConfig.addCollection("postsCollection", function (collection) {
     const tmpCollection = collection.getAllSorted();
     return tmpCollection.reverse().filter(function (tpl) {
       if (isPublishedPost(tpl.data)) {
@@ -65,10 +64,14 @@ module.exports = function (eleventyConfig) {
     });
   });
 
-  eleventyConfig.addCollection('bestOfCollection', function (collection) {
+  eleventyConfig.addCollection("bestOfCollection", function (collection) {
     const tmpCollection = collection.getAllSorted();
     return tmpCollection.reverse().filter(function (tpl) {
-      if (isPublishedPost(tpl.data) && tpl.data.tags && tpl.data.tags.includes('Best Of')) {
+      if (
+        isPublishedPost(tpl.data) &&
+        tpl.data.tags &&
+        tpl.data.tags.includes("Best Of")
+      ) {
         return true;
       }
     });
@@ -83,7 +86,7 @@ module.exports = function (eleventyConfig) {
     });
   });
 
-  eleventyConfig.addCollection('allTags', function (collection) {
+  eleventyConfig.addCollection("allTags", function (collection) {
     let allTags = [];
 
     collection.getAllSorted().forEach(function (el) {
@@ -104,62 +107,83 @@ module.exports = function (eleventyConfig) {
    * Filters
    */
 
-  eleventyConfig.addFilter('dateformat', function (dateIn) {
-    return moment(dateIn).format('MMM DD, YYYY');
+  eleventyConfig.addFilter("dateformat", function (dateIn) {
+    return moment(dateIn).format("MMM DD, YYYY");
   });
 
-  eleventyConfig.addFilter('keys', function (data) {
+  eleventyConfig.addFilter("keys", function (data) {
     return Object.keys(data);
   });
 
-  eleventyConfig.addFilter('json', function (data) {
+  eleventyConfig.addFilter("json", function (data) {
     return JSON.stringify(data, null, 2);
   });
 
-  eleventyConfig.addFilter('slug', function (text) {
+  eleventyConfig.addFilter("slug", function (text) {
     return makeSlug(text);
   });
 
-  eleventyConfig.addFilter('tweetIdeaUrl', function (text) {
-    const tweetText = `Hey @joshcanhelp, I want ${text}! https://www.joshcanhelp.com/ideas#${makeSlug(text)}`;
-    return "https://twitter.com/intent/tweet?text=" + encodeURIComponent(tweetText);
+  eleventyConfig.addFilter("tweetIdeaUrl", function (text) {
+    const tweetText = `Hey @joshcanhelp, I want ${text}! https://www.joshcanhelp.com/ideas#${makeSlug(
+      text
+    )}`;
+    return (
+      "https://twitter.com/intent/tweet?text=" + encodeURIComponent(tweetText)
+    );
   });
 
   /*
    * Shortcodes
    */
 
-  eleventyConfig.addPairedShortcode('h2br', (text) =>
-    `<h2 class="hr aligncenter" id="${makeSlug(text)}">
+  eleventyConfig.addPairedShortcode(
+    "h2br",
+    (text) =>
+      `<h2 class="hr aligncenter" id="${makeSlug(text)}">
       <span class="pink">&lt;</span>
         ${text}
       <span class="pink">&gt;</span>
     </h2>`
   );
 
-  eleventyConfig.addPairedShortcode('info', (text) =>
-    `<blockquote class="info-block">${text}</blockquote>`
+  eleventyConfig.addPairedShortcode(
+    "info",
+    (text) => `<blockquote class="info-block">${text}</blockquote>`
   );
 
-  eleventyConfig.addPairedShortcode('warning', (text) =>
-    `<blockquote class="warning-block">${text}</blockquote>`
+  eleventyConfig.addPairedShortcode(
+    "warning",
+    (text) => `<blockquote class="warning-block">${text}</blockquote>`
   );
 
-  eleventyConfig.addPairedShortcode('twitter', (url) =>
-    `<p><a href="{{ url }}" class="tag">Discuss on Twitter &rsaquo;</a></p>`
+  eleventyConfig.addPairedShortcode(
+    "twitter",
+    (url) =>
+      `<p><a href="{{ url }}" class="tag">Discuss on Twitter &rsaquo;</a></p>`
   );
 
   return {
     dir: {
       input: "_content",
-      output: "_dist"
+      output: "_dist",
     },
     templateFormats: [
-      'md', 'css', 'html', 'txt', 'ico', 'png', 'jpg', 'gif', 'htaccess', 'pdf', 'toml', 'njk'
+      "md",
+      "css",
+      "html",
+      "txt",
+      "ico",
+      "png",
+      "jpg",
+      "gif",
+      "htaccess",
+      "pdf",
+      "toml",
+      "njk",
     ],
-    passthroughFileCopy: true
+    passthroughFileCopy: true,
   };
 };
 
-const isPublishedPost = (data) => data.permalink && 'post' === data.layout;
-const makeSlug = (text) => slugify(text, { lower: true, strict: true })
+const isPublishedPost = (data) => data.permalink && "post" === data.layout;
+const makeSlug = (text) => slugify(text, { lower: true, strict: true });
