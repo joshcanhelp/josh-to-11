3426fc8ad9e2823cd1d6c6f0ca7c25390da51746
Josh Cunningham <josh@joshcanhelp.com>
2021-12-11 12:40:52 -0800

    feat: dynamically generate post date from file name

---
+++ input/posts/posts.11tydata.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+/*
+console.log(data.page);
+{
+  date: 2021-11-01T12:00:00.000Z,
+  inputPath: './input/posts/2021/2021-11-01-taking-notes.md',
+  fileSlug: 'taking-notes',
+  filePathStem: '/posts/2021/taking-notes',
+  url: '/notes/',
+  outputPath: '_dist/notes/index.html'
+}
+*/
+
+module.exports = {
+  eleventyComputed: {
+    date: data => {
+      const { inputPath, fileSlug } = data.page;
+      const inputPathParts = inputPath.split("/");
+      const fileName = inputPathParts[inputPathParts.length - 1];
+      const postDate = fileName.replace(`-${fileSlug}.md`, "");
+      return postDate + " 12:00:00"
+    }
+  }
+};
+
