7e213d216511b5d7220ebd6ac2b4c34870e8617e
Josh Cunningham <josh@joshcanhelp.com>
2024-09-06 10:16:07 -0700

    feat: move API from flat file to Netlify func

--- netlify_functions/hello.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async function (event, context) {
-  console.dir(event);
-  console.dir(context);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
-};
